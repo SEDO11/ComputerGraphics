78dfa2b0819efee077995577b1cc4948160eb725
SEDO11 <do11anm@naver.com>
2021-10-19 14:09:49 +0900

    211019

---
+++ exam6_8/exam6_8/exam6_8.cpp
@@ -0,0 +1,68 @@
+﻿//무조건 glut를 처음에 넣어줘야 한다.
+#include <gl/glut.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+//조명에 관한 함수
+void InitLight() {
+	GLfloat mat_diffuse[] = { 0.5, 0.4, 0.3, 1.0 };
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_ambient[] = { 0.5, 0.4, 0.3, 1.0 };
+	GLfloat mat_shininess[] = { 15.0 };
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat light_ambient[] = { 0.3, 0.3, 0.3, 1.0 };
+	GLfloat light_position[] = { -3, 6, 3.0, 0.0 };
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+//화면
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 1.0, 0.0);
+	glutSolidTeapot(0.5);
+	glFlush();
+}
+
+void MyReshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
+//6-9 극좌표 코드
+void PolarView(GLfloat radius, GLfloat elevation, GLfloat azimuth, GLfloat twist) {
+	glTranslatef(0.0, 0.0, -radius); //시점 좌표계 -> 전역 좌표계
+	glRotatef(-twist, 0.0, 0.0, 1.0); //시점 좌표계 축을 기준으로 회전이 가해짐
+	glRotatef(-elevation, 1.0, 0.0, 0.0); // 상향벡터 조절
+	glRotatef(-azimuth, 0.0, 1.0, 0.0);
+}
+
+//실행
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("OpenGL Sample Drawing");
+	glClearColor(0.4, 0.4, 0.4, 0.0);
+	InitLight();
+	PolarView(10.0, 5.0, 1.0, 7.0);
+	glutDisplayFunc(MyDisplay);
+	glutReshapeFunc(MyReshape);
+	glutMainLoop();
+}
